11e44d8f7110ad646d3d226fa44a9c8869c05af1
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-04-25 15:43:37 -0500

    set Backend and pin provider version

--- providers.tf
+++ providers.tf
@@ -1,6 +1,8 @@
 # Import GitHub resources
 
 provider "github" {
+  version = "~> 2.6"
+
   # This is the target GitHub organization to manage
   organization = var.github_organization
 
--- terraform.tf
+++ terraform.tf
@@ -2,4 +2,11 @@
 
 terraform {
   required_version = "~> 0.12"
+
+  # https://www.terraform.io/docs/backends/types/s3.html
+  backend "s3" {
+    bucket = "andys-terraform-backend"
+    region = "us-east-1"
+    key    = "tf-gh-org/"
+  }
 }
