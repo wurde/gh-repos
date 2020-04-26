58b2b6640324a422ba87c6105c4b6a7c0e416eb6
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-04-25 21:36:42 -0500

    Fix project def and add repo

--- projects.tf
+++ projects.tf
@@ -9,22 +9,22 @@ resource "github_organization_project" "default" {
 
 
 resource "github_project_column" "backlog" {
-  project = github_organization_project.default.id
-  name    = "Backlog"
+  project_id = github_organization_project.default.id
+  name       = "Backlog"
 }
 
 resource "github_project_column" "to_do" {
-  project = github_organization_project.default.id
-  name    = "To Do"
+  project_id = github_organization_project.default.id
+  name       = "To Do"
 }
 
 resource "github_project_column" "in_progress" {
-  project = github_organization_project.default.id
-  name    = "In Progress"
+  project_id = github_organization_project.default.id
+  name       = "In Progress"
 }
 
 resource "github_project_column" "done" {
-  project = github_organization_project.default.id
-  name    = "Done"
+  project_id = github_organization_project.default.id
+  name       = "Done"
 }
 
---
+++ repositories.tf
@@ -0,0 +1,22 @@
+/**
+ * Define Org repositories.
+ */
+
+resource "github_repository" "example" {
+  name        = "example"
+  description = "An MIT LICENSE file."
+
+  topics = ["terraform", "github", "infrastructure-as-code"]
+
+  default_branch     = "master"
+  gitignore_template = "Node"
+  license_template   = "mit"
+
+  private      = false
+  has_issues   = false
+  has_projects = false
+  has_wiki     = false
+
+  delete_branch_on_merge = true
+  auto_init              = true
+}
