436e8cca1ce6860484f7f3e0a5bbf07a2b8fec3d
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-04-25 20:19:42 -0500

    let default GITHUB_TOKEN

--- providers.tf
+++ providers.tf
@@ -5,7 +5,4 @@ provider "github" {
 
   # This is the target GitHub organization to manage
   organization = var.github_organization
-
-  # This is the GitHub personal access token.
-  token = var.github_token
 }
--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "github_organization" {
   description = "The GitHub Organization to manage."
 }
 
-variable "github_token" {
-  type        = string
-  description = "The GitHub personal access token."
-}
-
 variable "membership" {
   default = {
     "MosesSupposes"   = "admin"
