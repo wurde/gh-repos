1289edd9635db85cac85489f4295d444a9dd4349
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-04-25 21:16:03 -0500

    Add sillyhats Team

---
+++ teams.tf
@@ -0,0 +1,26 @@
+/**
+ * Define GitHub Teams.
+ */
+
+resource "github_team" "sillyhats" {
+  name = "sillyhats"
+  description = "https://amazon.com/funny-hats"
+}
+
+/**
+ * Define Team Membership.
+ */
+
+resource "github_team_membership" "sillyhats_maintainer" {
+  team_id = github_team.sillyhats.id
+  username = "wurde"
+  role = "maintainer"
+}
+
+resource "github_team_membership" "sillyhats_members" {
+  for_each = var.membership
+
+  team_id = github_team.sillyhats.id
+  username = each.key
+  role = "member"
+}
